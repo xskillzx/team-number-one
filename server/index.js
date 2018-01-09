267b7017195c4924519c5bc490822c0e059a9d94
justin <justindulark@gmail.com>
2018-01-09 13:49:18 -0800

    Change port to look for dyno

--- server/index.js
+++ server/index.js
@@ -8,10 +8,9 @@ var app = express();
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!')
+  res.send('Hello, World!');
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
-});
 
+var port = process.env.PORT || 8080;
+app.listen(port);
