ef1691edc7b7a7e399bb2ce7b7846d0725a7bba2
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-11 11:39:26 -0800

    Creates UserInfo component

---
+++ react-client/src/components/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserInfo = function(props) {
+	return (
+		<div class="userinfo">
+			<h1>THIS IS THE USER INFO COMPONENT OMG</h1>
+		</div>
+	)
+};
+
+export default UserInfo;
---
+++ react-client/src/containers/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserInfo from '../components/UserInfo.jsx';
+
+class HomePage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+
+	}
+
+	render() {
+		return(
+			<UserInfo />
+		)
+	}
+}
+
+export default HomePage;
