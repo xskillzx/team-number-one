8a11e8d9a601a4dbed45eee7d1b7f23d2d3c4948
justin <justindulark@gmail.com>
2018-01-11 12:07:27 -0800

    Refactor database connection

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
 
-const connection = mysql.createConnection(mysqlConfig);
-
 var connection;
 if (process.env.JAWSDB_URL) {
   //Heroku deployment
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   //local host
-    connection = mysql.createConnection(mysqlConfig);
+  connection = mysql.createConnection(mysqlConfig);
+}
