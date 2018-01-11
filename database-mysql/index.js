67793f3fc86df7c5747d6d2f4a8967b558894ddd
justin <justindulark@gmail.com>
2018-01-11 10:49:55 -0800

    HAndle server connection depending on environment

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -2,3 +2,11 @@ const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection(mysqlConfig);
