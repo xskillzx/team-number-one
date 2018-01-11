b7d688a95700d5c483afd313ab589361158112d1
hchesnutt <t.henry.chesnutt@gmail.com>
2018-01-11 12:26:44 -0800

    connect post handlers between WritePost and HomePage

--- react-client/src/components/WritePost.jsx
+++ react-client/src/components/WritePost.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 const WritePost = (props) => (
   <div className="post">
-    <form>
-      <label>
-        Post a Squeak:
-        <input
-          type="text"
-          name="name"
-          value={props.writePostValue}
-          onChange={props.onPostInputChangeHandler}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <label>
+      Post a Squeak:
+      <input
+        type="text"
+        name="name"
+        value={props.writePostValue}
+        onChange={props.onPostInputChangeHandler}
+      />
+    </label>
+    <input type="submit" value="Submit" onClick={props.writePostHandler} />
   </div>
 );
 
