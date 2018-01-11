df5064c006538e8065923a0678c363d25bb8c4d1
hchesnutt <t.henry.chesnutt@gmail.com>
2018-01-11 12:26:44 -0800

    connect post handlers between WritePost and HomePage

--- react-client/src/containers/HomePage.jsx
+++ react-client/src/containers/HomePage.jsx
@@ -46,8 +46,9 @@ class HomePage extends React.Component {
   }
 
   writePostHandler() {
-    // TODO
-    console.log('I was clicked');
+    // Create new squeak
+    // Send POST request
+    console.log(this.state.writePostValue);
   }
 
 >>>>>>> Relocates HomePage and SearchPage files to a containers folder and points files to the right directories
