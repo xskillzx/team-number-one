2b7308f5a41ec32417800bf9969d11cfc21d6134
justin <justindulark@gmail.com>
2018-01-11 10:23:23 -0800

    Add code to check for heroku jawsdb environmental variable

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -3,3 +3,11 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection(mysqlConfig);
+};
