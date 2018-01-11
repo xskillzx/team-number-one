554e13fd49937e1bd682579260b4dd06fa82e069
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-11 13:54:13 -0800

    Relocates HomePage and SearchPage files to a containers folder and points files to the right directories

--- react-client/src/containers/HomePage.jsx
+++ react-client/src/containers/HomePage.jsx
@@ -1,46 +1,61 @@
 import React from 'react';
-import UserInfo from '../components/UserInfo.jsx';
+import WritePost from '../components/WritePost.jsx';
 import { Grid, Row, Col } from 'react-bootstrap';
+import UserInfo from '../components/UserInfo.jsx';
 
 class HomePage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      writePostValue: ''
+    };
+  }
 
-		};
+  writePostHandler() {
+    // TODO
+    console.log('I was clicked');
+  }
 
-	}
+  onPostInputChangeHandler(e) {
+    this.setState({writePostValue: e.target.value});
+  }
 
-	render() {
-		return(
-		  <Grid>
-		    <Col className="col-md-3 show-box show-grid">
-		      <Row className="show-box">
-		        <UserInfo />
-		      </Row>
-		      <Row className="show-box">
-		        <h3>TRENDING</h3>
-		      </Row>
-		    </Col>
-		    <Col className="col-md-6 show-box show-grid">
-		      <Row className="show-box">
-		        <h3>POST</h3>
-		      </Row>
-		      <Row className="show-box">
-		        <h3>FEED</h3>
-		      </Row>
-		    </Col>
-		    <Col className="col-md-3 show-box show-grid">
-		      <Row className="show-box">
-		        <h3>WHO TO FOLLOW</h3>
-		      </Row>
-		      <Row className="show-box">
-		        <h3>BOTTOM NAV</h3>
-		      </Row>
-		    </Col>
-		  </Grid>
-		)
-	}
+  render() {
+    return (
+    <Grid>
+      <Col className="col-md-3 show-box show-grid">
+        <Row className="show-box">
+          <UserInfo />
+        </Row>
+        <Row className="show-box">
+          <h3>TRENDING</h3>
+        </Row>
+      </Col>
+      <Col className="col-md-6 show-box show-grid">
+        <Row className="show-box">
+          <div className="home">
+            <WritePost
+              writePostValue={this.state.writePostValue}
+              onPostInputChangeHandler={this.onPostInputChangeHandler.bind(this)}
+              writePostHandler={this.writePostHandler.bind(this)}
+            />
+          </div>
+        </Row>
+        <Row className="show-box">
+          <h3>FEED</h3>
+        </Row>
+      </Col>
+      <Col className="col-md-3 show-box show-grid">
+        <Row className="show-box">
+          <h3>WHO TO FOLLOW</h3>
+        </Row>
+        <Row className="show-box">
+          <h3>BOTTOM NAV</h3>
+        </Row>
+      </Col>
+    </Grid>
+    );
+  }
 }
 
 export default HomePage;
--- react-client/src/containers/SearchPage.jsx
+++ react-client/src/containers/SearchPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import $ from 'jquery';
-import Feed from './Feed.jsx';
+import Feed from '../components/Feed.jsx';
 
 class SearchPage extends React.Component {
   constructor(props) {
