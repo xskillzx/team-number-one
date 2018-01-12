0fc74548711ea1209513a839142931d9b843baef
Felipe Catania <felicatania@gmail.com>
2018-01-11 18:55:09 -0800

    Add logs for travis debugging

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -67,6 +67,8 @@ describe('Persistent Node Squeaker Server', function() {
 	});
 
 	it('The db should return something when hitting /api/search', done => {
+		console.log(process.env);
+		console.log(process.env.PORT);
 		let uri = process.env.PORT ? '/api/search?q=fe' : 'http://127.0.0.1:3000/api/search?q=fe';
 		// let port = process.env.PORT || 3000;
 		request(uri, (error, response, body) => {
