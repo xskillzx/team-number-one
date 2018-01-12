450cc49f59af030fd5cecc0b4d79f6c4bb44ded1
hchesnutt <t.henry.chesnutt@gmail.com>
2018-01-12 10:39:18 -0800

    refactor writepost request to axios

---
+++ react-client/specs/homepage.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import { spy } from 'sinon';
+import HomePage from '../src/containers/HomePage.jsx';
+
+describe('Test HomePage', () => {
+  it('Test Case #1', () => {
+    // actual test
+  });
+  it('Test Case #2', () => {
+    // actual test
+  });
+});
