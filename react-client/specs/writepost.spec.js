8055df4b886822a0bf2c09372d9bf1730dbf267c
hchesnutt <t.henry.chesnutt@gmail.com>
2018-01-11 20:13:12 -0800

    add and pass tests for writepost

---
+++ react-client/specs/writepost.spec.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import WritePost from '../src/components/WritePost.jsx';
+
+describe('Test WritePost', () => {
+  it('WritePost\'s Post button should call the passed in function', () => {
+    let mockWritePostHandler = jest.fn();
+    const writepost = shallow(<WritePost
+      writePostValue='not important here'
+      writePostHandler={mockWritePostHandler}
+    />);
+    writepost.find({type: 'submit'}).simulate('click');
+    expect(mockWritePostHandler.mock.calls.length).toEqual(1);
+  });
+
+  it('When WritePost input changes, it should call passed in changeHandler', () => {
+    let mockInputChangeHandler = jest.fn();
+    const writepost = shallow(<WritePost
+      onPostInputChangeHandler={mockInputChangeHandler}
+    />);
+    // check if mock function was called when input value changes
+    // console.log(writepost.find({type: 'text'}));
+    writepost.find({type: 'text'}).simulate('change');
+    expect(mockInputChangeHandler.mock.calls.length).toEqual(1);
+  });
+
+  it('When hitting enter in the input, the post handler should be called', () => {
+    let mockInputChangeHandler = jest.fn();
+    const writepost = shallow(<WritePost
+      writePostHandler={mockInputChangeHandler}
+    />);
+
+    writepost.find({type: 'text'}).simulate('keyPress', {key: 'Enter'});
+    expect(mockInputChangeHandler.mock.calls.length).toEqual(1);
+  });
+});
