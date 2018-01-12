3cd4d821f14241683b433059bd1bdd0eb6872b7d
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-12 15:11:50 -0800

    Complete squeak entry and feed functionality with some styling changes done to the overall site. Added momentjs as a dependancy for user friendly time display

--- react-client/src/components/SqueakEntry.jsx
+++ react-client/src/components/SqueakEntry.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Media } from 'react-bootstrap';
+import { Media, Image } from 'react-bootstrap';
+import moment from 'moment';
 
 const SqueakEntry = ({squeak}) => (
   <Media className="squeak-entry">
     <Media.Right>
-      <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
+      <Image width={64} height={64} src={squeak.profile_img_url} alt="rounded" rounded/>
     </Media.Right>
     <Media.Body>
-      <Media.Heading>Media Heading</Media.Heading>
+      <Media.Heading>@{squeak.username}</Media.Heading>
+      <p className="squeak-name-time">
+        {squeak.display_name} &#183; {moment(squeak.created_at).fromNow()}
+      </p>
       <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus.
+        {squeak.text}
       </p>
     </Media.Body>
   </Media>
