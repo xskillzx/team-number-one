49f66388d50a01491467e73b14e4800eb8d044c5
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-12 12:14:48 -0800

    Add componentDidMount function to HomePage to GET all squeaks from srever->db and generate squeaks into the feed per data retrieved

--- react-client/src/components/Feed.jsx
+++ react-client/src/components/Feed.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import SqueakEntry from './SqueakEntry.jsx';
 import UserEntry from './UserEntry.jsx';
 
-const Feed = ({ squeaks, users }) => (
-  <div>
+const Feed = (props) => (
+<div className="feed-container">
+  {props.squeaks.map(squeak => <SqueakEntry key={squeak.id} squeak={squeak} />)}
+</div>
+);
+
+export default Feed;
+
+/*  <div>
     {squeaks &&
       squeaks.map(squeak => (
         <SqueakEntry key={squeak.id} squeak={squeak}/>
@@ -13,7 +20,4 @@ const Feed = ({ squeaks, users }) => (
       users.map(user => (
         <UserEntry {...user} key={user.id}/>
       ))}
-  </div>
-);
-
-export default Feed;
+  </div>*/
