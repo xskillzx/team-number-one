f8dd15362a2b40910e77e1ec1502f876cc712896
Felipe Catania <felicatania@gmail.com>
2018-01-12 20:27:11 -0800

    Send follow handlers down to UserEntry

--- react-client/src/components/UserList.jsx
+++ react-client/src/components/UserList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import UserEntry from './UserEntry.jsx';
 
-const UserList = ({ users }) => (
+const UserList = ({ users, followHandler, unfollowHandler }) => (
   <div className="userlist-container">
-    {users && users.map(user => <UserEntry key={user.id} {...user}/>)} 
+    {users && users.map(user => <UserEntry key={user.id} {...user} followHandler={followHandler} unfollowHandler={unfollowHandler}/>)} 
   </div>
 );
 
