a16c0aa7dd21b6d1aa3293a4d1b8b0adff702357
justin <justindulark@gmail.com>
2018-01-13 08:28:40 -0800

    Reorder loading sheets and delete unnecessary file'

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -30,11 +30,6 @@ class App extends React.Component {
     };
   }
 
-
-  loggedIn() {
-    return this.state.loggedIn;
-  }
-
   searchHandler() {
     if (this.state.inputValue.length > 0) {
       this.props.history.push(`/search?q=${this.state.inputValue}`);
@@ -88,14 +83,15 @@ class App extends React.Component {
           userpic={this.state.userinfo[0].profile_img_url}
         />
         <Switch>
-          {/* <Route exact path="/" render={props => (<HomePage/>)}/> */}
-          <Route exact path="/" render={props => (
+          <Route exact path="/" render={props => (<HomePage/>)}/>
+          {/* <Route exact path="/" render={props => (
             this.state.loggedIn ? (
               <Redirect to="/login"/>
             ) : (
               <HomePage/>
             )
-          )}/>
+          )}/> */}
+          <Redirect from="/find" to="/search"/>
           <Route path="/search" render={props => (<SearchPage {...props.location}/>)}/>
           <Route path="/login" render={props => (<LoginPage/>)}/>
           <Route path="/:username" render={props => (<HomePage username={props.match.params.username}/>)}/>
@@ -108,9 +104,9 @@ class App extends React.Component {
 
 App = withRouter(App);
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('app')
-);
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>, document.getElementById('app')
+// );
 
