f54a5b9eed857e51e288fea40640d73d2ea6cd5d
Felipe Catania <felicatania@gmail.com>
2018-01-13 15:26:14 -0800

    Send down user to SqueakEntry

--- react-client/src/components/Feed.jsx
+++ react-client/src/components/Feed.jsx
@@ -3,7 +3,7 @@ import SqueakEntry from './SqueakEntry.jsx';
 
 const Feed = (props) => (
   <div className="feed-container">
-    {props.squeaks && props.squeaks.map(squeak => <SqueakEntry key={squeak.id} squeak={squeak} />)}
+    {props.squeaks.map(squeak => <SqueakEntry key={squeak.id} squeak={squeak} user={props.user}/>)}
   </div>
 );
 
