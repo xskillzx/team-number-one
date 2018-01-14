7ea94ca3b5c89eba90f00c4717b29d6aba4f9d06
Moises Munoz <moises_a_munoz@yahoo.com>
2018-01-13 19:13:49 -0800

    Implementing Carousel

--- react-client/src/components/WhoToFollow.jsx
+++ react-client/src/components/WhoToFollow.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, Image } from 'react-bootstrap';
+import { Panel, Image, Carousel } from 'react-bootstrap';
 
 const WhoToFollow = function(props) {
 	return (
@@ -10,37 +10,12 @@ const WhoToFollow = function(props) {
 				</Panel.Title>
 			</Panel.Heading>
 			<Panel.Body className="whotofollow-body">
-			  <div className="whotofollow-people">
 			    <Carousel>
+			      { console.log(props.top) }
 			    </Carousel>
-				</div>
 			</Panel.Body>
 		</Panel>
 	)
 };
 
 export default WhoToFollow;
-
-/*	<Carousel>
-		<Carousel.Item>
-			<img width={900} height={500} alt="900x500" src="/carousel.png" />
-			<Carousel.Caption>
-				<h3>First slide label</h3>
-				<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-			</Carousel.Caption>
-		</Carousel.Item>
-		<Carousel.Item>
-			<img width={900} height={500} alt="900x500" src="/carousel.png" />
-			<Carousel.Caption>
-				<h3>Second slide label</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			</Carousel.Caption>
-		</Carousel.Item>
-		<Carousel.Item>
-			<img width={900} height={500} alt="900x500" src="/carousel.png" />
-			<Carousel.Caption>
-				<h3>Third slide label</h3>
-				<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-			</Carousel.Caption>
-		</Carousel.Item>
-	</Carousel>*/
